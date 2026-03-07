9487dc1a100e5ea5ff52e9e99b5a7b59e46163b1
João Barreto <Joo.bcsilva@upe.br>
2026-03-07 19:02:45 -0300

    Atualizar testeWhile.

--- testeWhile.c
+++ testeWhile.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 
 int main() {
-	char stop[3];
+	char stop[3]; 
+	int n1, n2;
 
 	while (1) {
-		printf("Do you want to keep on? (y/n)");
-		fgets(stop, sizeof(stop), stdin); 
+		printf("Type in two numbers, wich will be added together.\n");
+		
+		printf("Number 1: ");
+		scanf("%d", &n1);	
 
+		while (getchar() != '\n');
+		
+		printf("Number 2: ");
+		scanf("%d", &n2);
+
+		while (getchar() != '\n');
+
+		printf("\nThe sum of n1 and n2 equals to: %d.", n1 + n2);
+
+		printf("\nDo you want to keep on? (y/n)");
+		fgets(stop, sizeof(stop), stdin);
+		
 		if (stop[0] == 'n' || stop[0] == 'N') {
 			break;
 		}
+
+		printf("\n");
 	}
 
 	return 0;	
