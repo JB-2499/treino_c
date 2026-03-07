63d3fa4c2b799cf7a1b7e5431db28a28d1ecf86a
João Barreto <Joo.bcsilva@upe.br>
2026-03-07 14:50:09 -0300

    corrigir arquivos

---
+++ challenge.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int itemPrice = 10;
+	int shippingCost = 5;
+	
+	int sum = (itemPrice + shippingCost);
+	
+	printf("Sum = %d", sum);
+}
--- main.c
+++ main.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+#include <locale.h>
 
 int main() {
+	setlocale(LC_ALL, "Portuguese");
+
+	char nome[40];
+	char curso[30];
 	int idade;
+	
+
+	printf("Digite seu nome completo: ");
+	fgets(nome, 40, stdin);
+
+	nome[strcspn(nome, "\n")] = 0;
 
 	printf("Digite sua idade: ");
 	scanf("%d", &idade);
+	while(getchar() != '\n');
 
-	printf("Sua idade é: %d", idade);
+	printf("Digite o a graduação que faz atualmente: ");
+	fgets(curso, 30, stdin);
+
+	curso[strcspn(curso, "\n")] = 0;
+
+	printf("Seu nome é: %s.\n", nome);
+	printf("Sua idade é: %d.\n", idade);
+	printf("Seu curso de graduação é: %s.", curso);
 	
 	return 0;
 }
